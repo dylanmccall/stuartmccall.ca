fc1c9cf9a2991a92bc7ee05e3d8d461a5d7f0291
Dylan McCall <dylan@dylanmccall.com>
2017-08-02 14:04:41 -0700

    Add a package with some helpful admin commands

--- artsite/settings.py
+++ artsite/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'django_extensions',
     'orderable',
     'sorl.thumbnail',
     'simplemde',
